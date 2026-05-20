c99fcb827285fe36591a1888b0540a4f634805db
vishnugettam <vishnugettam@gmail.com>
2026-05-21 00:31:35 +0530

    file handling code

---
+++ an.py
@@ -0,0 +1,4 @@
+import sample as sp
+
+displaydata = sp.Addition(5,6);
+print(displaydata);
---
+++ sample.py
@@ -0,0 +1,12 @@
+def Addition(a:int,b:int):
+    print("__name__ : " ,__name__)
+    print("Addition of {} and {} is - {} ".format(a,b,a+b));
+    
+    
+
+
+if __name__ == "__main__":
+    val = Addition(4,7);
+
+    print(val);
+
