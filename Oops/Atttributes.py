904a7300dd67a147f711957cce1978d1bb4df786
vishnugettam <vishnugettam@gmail.com>
2026-05-21 22:02:15 +0530

    method types

---
+++ Oops/Atttributes.py
@@ -0,0 +1,34 @@
+"""
+    Types of attributes 
+
+        Object  -> object specific 
+        Class   -> same for all objects
+
+"""
+
+
+
+class Person:
+    # class parameters
+    city = "Bangalore"
+    
+    
+    def __init__(self,name,age):
+    # instance or object parameters
+        self.name = name
+        self.age = age
+        
+    def GetValue(self):
+        # call class params with "self"
+        print(f"  Use Name : {self.name}  && Age : {self.age}  && city : {self.city} ")
+        
+        #call class params with Person (class name)
+        print(f"  Use Name : {self.name}  && Age : {self.age}  && city : {Person.city} ")
+        
+        
+
+if __name__ == "__main__":
+    p1 = Person('Vishnu',20)
+    
+    p1.GetValue()
+    
---
+++ Oops/Methods.py
@@ -0,0 +1,38 @@
+"""
+Methods : 
+     Static method   -> only "self" as input params
+     Instance method -> can have its own params
+"""
+
+# The class `Person` defines an instance method `GetValue` to display the name and age of a person,
+# and a static method `GetValue_StaticMethod` for static method execution.
+
+class Person:
+    
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+            
+    def GetValue(self):
+        print(f" Display from Instance Method : \n  UserName : {self.name} && Age : {self.age}  ")
+    
+    
+    @staticmethod
+    def GetValue_StaticMethod():
+        print(f"Static Method execution")
+
+
+
+if __name__ == "__main__":
+    p1 = Person(name="Vihaan",age=8)
+    
+    p1.GetValue()
+    
+    p1.GetValue_StaticMethod()
+    
+    
+    p2 = Person("Vishnu",4)
+    
+    p2.GetValue()
+    
+    p2.GetValue_StaticMethod()
