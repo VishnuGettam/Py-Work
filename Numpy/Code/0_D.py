ec289f5e86abcfbc6d9e05bbf05af1c9949f58ab
vishnugettam <vishnugettam@gmail.com>
2026-07-14 18:23:18 +0530

    Numpy

---
+++ Numpy/Code/0_D.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+a = np.array(35)
+
+print("----------O_D Array-------------")
+print(f"Array  : {a}")
+print(f"Array dimensions : {a.ndim}")
+print(f"Array shape : {a.shape}")
+print(f"Array size : {a.size}")
+
+ 
